ed43119258bf0436dfef5195729a0c74cd5fc4bc
Rafael <roboticra@gmail.com>
2023-05-05 23:39:35 -0500

    okay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  fetch("https://gamespiapi-production.up.railway.app/genres", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(response));
   return (
     <div className="App">
       <h1>Henry Videogames</h1>
