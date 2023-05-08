5bf674b8811ab5c2debf6923838105ce3bf907fe
Rafael <roboticra@gmail.com>
2023-05-07 23:44:55 -0500

    fix url

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,4 @@
-const API_URL = "https://gamespiapi-production.up.railway.app/";
+const API_URL = "https://gamespiapi-production.up.railway.app";
 
 export const GET_GENRES = "GET_GENRES";
 export const GET_VIDEOGAMES = "GET_VIDEOGAMES";
